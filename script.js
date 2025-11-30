60bcfa65fd3c7d1ae2183e33678bd383905059ca
c4t <142375208+imalwaysad@users.noreply.github.com>
2025-11-29 19:52:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,40 @@ submitBtn.addEventListener("click", () => {
         
     }, 5000); // verifying for 5 seconds
 });
+
+const notification = document.getElementById("notification");
+
+// Random usernames AI-created
+const usernames = [
+    "SABGAGGOD124",
+    "TungTungTung_12",
+    "GrowAGardenMaster47",
+    "Meowel_sab16",
+    "MrBrainrot145",
+    "GAGRacoon1",
+    "StealABrainrot123",
+    "FreeTungSahur4",
+    "MutatedNoobini23",
+    "Noobini_pizzanini12"
+];
+
+// Random flag emojis
+const flags = ["🇺🇸","🇬🇧","🇫🇷","🇩🇪","🇮🇹","🇨🇦","🇦🇺","🇧🇷","🇯🇵","🇪🇸","🇲🇽","🇮🇳"];
+
+function showNotification() {
+    let user = usernames[Math.floor(Math.random() * usernames.length)];
+    let flag = flags[Math.floor(Math.random() * flags.length)];
+
+    notification.innerHTML = `User <strong>${user}</strong> Claimed! ${flag}`;
+
+    // Slide in
+    notification.classList.add("show");
+    notification.classList.remove("hidden");
+
+    // Fade + slide out after 2 seconds
+    setTimeout(() => {
+        notification.classList.remove("show");
+        notification.classList.add("hidden");
+    }, 2000);
+}
+
